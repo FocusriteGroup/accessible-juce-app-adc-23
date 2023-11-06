76d1203508081c7c523910a1d1bcfeef4c4b28d9
Harry Morley <harry.morley@focusrite.com>
2023-11-06 11:51:57 +0000

    Support toggle button role

--- src/Button.cpp
+++ src/Button.cpp
@@ -1,2 +1,11 @@
 
 #include "Button.h"
+#include <juce_gui_basics/detail/juce_ButtonAccessibilityHandler.h>
+
+std::unique_ptr<juce::AccessibilityHandler> Button::createAccessibilityHandler ()
+{
+    return std::make_unique<juce::detail::ButtonAccessibilityHandler> (*this,
+                                                                       isToggleable ()
+                                                                           ? juce::AccessibilityRole::toggleButton
+                                                                           : juce::AccessibilityRole::button);
+}
--- src/Button.h
+++ src/Button.h
@@ -7,5 +7,8 @@ class Button : public juce::TextButton
 public:
     using TextButton::TextButton;
 
+private:
+    std::unique_ptr<juce::AccessibilityHandler> createAccessibilityHandler () override;
+
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Button)
 };
